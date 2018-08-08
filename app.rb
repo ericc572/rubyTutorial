85eeee6e4238f8eb2f400bed951bbbd075eda3e7
Eric Chen <echen1@salesforce.com>
2018-08-08 11:45:31 -0700

    APi calls but not working

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 # app.rb
 require "sinatra"
+require 'json'
+require 'net/http'
+
+url = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
+uri = URI.parse(URI.encode(url))
+api_response = Net::HTTP.get(uri)
+response = JSON.parse(api_response)
+price = response["data"]["amount"]
+
 class App < Sinatra::Base
   get "/" do
+    @price = params[:price]
     erb :index
   end
 end
