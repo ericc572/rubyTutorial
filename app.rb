7f4d1c546f67a2c4bd38e4c8aad213675164c971
Eric Chen <echen1@salesforce.com>
2018-08-08 11:58:12 -0700

    Finally working current btc price

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require "sinatra"
 require 'json'
 require 'net/http'
 
-url = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
-uri = URI.parse(URI.encode(url))
-api_response = Net::HTTP.get(uri)
-response = JSON.parse(api_response)
-price = response["data"]["amount"]
+
 
 class App < Sinatra::Base
   get "/" do
-    @price = params[:price]
+    url = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
+    uri = URI.parse(URI.encode(url))
+    api_response = Net::HTTP.get(uri)
+    response = JSON.parse(api_response)
+    @price = response["data"]["amount"]
     erb :index
   end
 end
