b4f4fc8f4179dc13e33db0eff80e0db0d32807cf
Eric Chen <echen1@salesforce.com>
2018-08-23 16:35:03 -0700

    following wordpress tutorial to build db driven stuff

--- app.rb
+++ app.rb
@@ -1,16 +1,14 @@
 # app.rb
 require "sinatra"
+require 'sinatra/activerecord'
 require 'json'
+require './config/environments' #database configuration
+require './models/model'
 
-class App < Sinatra::Base
-  get "/" do
-    erb :index
-  end
+get '/' do
+  erb:index
+end
 
-  post "/moods" do
-    puts params
-    keyword = params["keyword"]
-    @price = Prices.search_prices(keyword)
-    erb :views
-  end
+post '/create' do
+  "Created a user"
 end
---
+++ db/migrate/20180823233433_create_model.rb
@@ -0,0 +1,4 @@
+class CreateModel < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
